f9d0e3c6077862f6586c4f5fd1f7726480464241
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2021-03-20 07:12:15 -0300

    Projeto criado

---
+++ src/main/java/com/marcelo/pesquisando/PesquisandoApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.pesquisando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisandoApplication.class, args);
+	}
+
+}
